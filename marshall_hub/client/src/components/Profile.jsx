04ba924720eb4bdd2a35cb2d2f7d462e6e84baa9
harleydi <disneyharley26@gmail.com>
2020-03-20 13:55:42 -0400

    added css

--- marshall_hub/client/src/components/Profile.jsx
+++ marshall_hub/client/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
-import { readUser } from './Services/api-helper'
+import { readUser, destroyCase } from './Services/api-helper'
 import { Link } from 'react-router-dom'
 
 
@@ -19,16 +19,26 @@ class Profile extends Component {
         console.log(res)
     }
 
+    handleDelete = async () => {
+        await destroyCase(this.props.match.params.caseid)
+        this.props.history.push('/profile') 
+    }
+
     renderUser = () => (
         <div className='profileCases'>
-            <img className='profileImg' src='https://place-hold.it/300' />
+            <Link className='profileEdit'>Edit Profile</Link>
+            <img className='profileImg' src='https://images.pexels.com/photos/2814239/pexels-photo-2814239.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' />
             <h1 className='username'>{this.state.user.username.toUpperCase()}</h1>
             <p className='casenum'>Total Cases: {this.state.user.cases.length}</p>
             {this.state.user.cases.map(Case => (
-                <div className='profile list'>
+                <div className='profileList'>
                     <Link to={`/cases/${Case.id}`} className='box-links'>
                         <div className='box box-text'>
                             <h2 className='casename'>{Case.victim_name}</h2>
+                            <div className='adminButtons'>
+                                <Link to={`/cases/${Case.id}/edit`} className='editBtn'><i class="fas fa-edit"></i></Link>
+                                <Link to={`/cases/${Case.id}/delete`} onClick={this.handleDelete} className='dltBtn'><i class="fas fa-trash-alt"></i></Link>
+                            </div>
                             
                         </div>
                     </Link>
